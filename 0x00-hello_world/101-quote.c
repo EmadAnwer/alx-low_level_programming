2af822cbc66d8402380f4f7815d9695aa07bd3e2
Emad Anwer <emadanwer.official@gmail.com>
2023-03-16 22:53:06 +0200

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return (1);
+}
