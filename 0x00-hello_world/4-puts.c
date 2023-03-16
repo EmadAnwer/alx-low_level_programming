8a778e4d31cfc49e0c8003dad430f78c397a9c08
Emad Anwer <emadanwer.official@gmail.com>
2023-03-16 21:23:51 +0200

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * prints "Programming is like building a multilingual puzzle with new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
