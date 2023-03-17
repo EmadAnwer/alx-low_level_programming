d1be3eec4507c0543ee846f2421afc87c6ea45f5
Emad Anwer <emadanwer.official@gmail.com>
2023-03-17 21:25:54 +0200

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/*
+ * Task 10 in 0x01. C - Variables, if, else, while project
+ * Purpose : prints all possible different combinations of two digits.
+ * my sulotion: nested loops
+ */
+
+/**
+ * main - function is the Entry Point for this program
+ * prints prints all possible combinations of single-digit numbers.
+ * 01, 02, 03, 04, 05, 06, 07, 08, 09, 12, 13, 14, 15...etc
+ * Return: value is (0)
+ */
+
+int main(void)
+{
+	int d2 = '0';
+
+	for (int d1 = '0'; d1 != ('8' + 1); d1++)
+	{
+		for (int d2 = '1'; d2 != ('9' + 1); d2++)
+		{
+		putchar(d1);
+		putchar(d2);
+		if (!((d1 == '8') && (d2 == '9')))
+			{
+			putchar(',');
+			putchar(' ');
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
