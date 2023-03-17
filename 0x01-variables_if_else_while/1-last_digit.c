43d96e3a509d8ad02dd500c795c159e67a5868b1
Emad Anwer <emadanwer.official@gmail.com>
2023-03-17 17:31:26 +0200

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * Task 1 in 0x01. C - Variables, if, else, while project
+ * Purpose : practice if and else if and how get last digit of the number
+ * Mission : checking if 'n' is positive or nigative or 0
+ *           get last digit of the number
+ * my sulotion: using Relational Operators  '< > ==' to check
+ *              get last digit of the number using %10
+ */
+
+/**
+ * main - function is the Entry Point for this porgram
+ * prints->
+ *	-Last digit of n is n%10 is  greater than 5
+ *	-Last digit of n is n%10 and is 0
+ *	-Last digit of n is n%10  and is less than 6 and not 0
+ * Return: value is (0)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n % 10 == 0)
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	else if (n % 10 < 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+
+	return (0);
+}
