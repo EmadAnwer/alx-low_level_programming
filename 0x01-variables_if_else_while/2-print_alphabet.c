5f764bbc0b40017821c17ab18d5178053f15c512
Emad Anwer <emadanwer.official@gmail.com>
2023-03-17 18:11:09 +0200

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -17,7 +17,7 @@ int main(void)
 	/*start from 97 means start from a becouse a = 97 in ascii */
 	char letter = 97;
 	/*letter != 123 "reapt untile letter == 122" in ascii 122 == 'z'   */
-	while(letter != 123)
+	while (letter != 123)
 	{
 		putchar(letter++);
 		putchar('\n');
