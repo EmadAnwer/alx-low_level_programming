f44a18f7428aca9efef67684473d204ba6061080
Emad Anwer <emadanwer.official@gmail.com>
2023-03-17 18:58:58 +0200

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * Task 5 in 0x01. C - Variables, if, else, while project
+ */
+
+/**
+ * main - function is the Entry Point for this porgram
+ * prints 0123456789
+ * Return: value is (0)
+ */
+
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
