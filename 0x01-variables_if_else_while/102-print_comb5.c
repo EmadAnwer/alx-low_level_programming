93b8645af489c1ed567d44a56050f8f9d46ee056
Emad Anwer <emadanwer.official@gmail.com>
2023-03-18 14:52:23 +0200

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,44 +1,49 @@
 #include <stdio.h>
 
 /*
- * Task 11 in 0x01. C - Variables, if, else, while project
- * Purpose : prints all possible different combinations of three digits
+ * Task 12 in 0x01. C - Variables, if, else, while project
+ * Purpose : prints all possible different combinations of 4 digits
  * my sulotion: nested loops
  */
 
 /**
  * main - function is the Entry Point for this program
  * prints all possible different combinations of three digits
- * 012, 013, 014, 015, 016, 017, 018, 019, 023, 024, 025, 026..etc
+ * 00 01, 00 02, 00 03, 00 04, 00 05, 00 06, 00 07,..etc
  * Return: value is (0)
  */
 
+#include <stdio.h>
 
 int main(void)
 {
-	int digit1, digit2, digit3;
+	int i, j;
 
-	for (digit1 = '0'; digit1 != ('7' + 1); digit1++)
+	for (j = 0; j <= 99; j++)
 	{
-		for (digit2 = digit1 + 1; digit2 != ('8' + 1); digit2++)
+		for (i = j + 1; i <= 99; i++)
 		{
-			for (digit3 = digit2 + 1; digit3 != ('9' + 1); digit3++)
+
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			putchar(' ');
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			if (j * 100 + i != 9899)
 			{
-
-			putchar(digit1);
-			putchar(digit2);
-			putchar(digit3);
-
-				if ((!((digit1 == '7') && (digit2 == '8') && (digit3 == '9'))))
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
 
 			}
+
+
+
+
+
 		}
 
 	}
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }
+
