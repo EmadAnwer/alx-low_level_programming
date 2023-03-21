893b964c1f19c4f8bbfa386f1adbb44f531f09a0
Emad Anwer <emadanwer.official@gmail.com>
2023-03-21 16:38:04 +0200

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,27 @@
 #include "main.h"
+/*
+ * Task 1 in 0x02. C - Functions, nested loops
+ * Purpose : including our own function in another file
+ * Mission : the alphabet, in lowercase, followed by a new line.
+ * my sulotion: using _putchar and while loops printing letters one by one
+ */
+
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ * using _putchar function
+ *
+ * Return: (void)
+ */
 
 void print_alphabet(void)
 {
 	char letter = 'a';
-	while(letter <= 'z')
+
+	while (letter <= 'z')
 	{
 		_putchar(letter++);
 	}
 	_putchar('\n');
+	return (void);
 }
