7d6486f8ddb3c84894320b7fc8a557e00b838957
Emad Anwer <emadanwer.official@gmail.com>
2023-03-21 17:28:45 +0200

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/*
+ * Task 2 in 0x02. C - Functions, nested loops
+ * Purpose : including our own function in another file
+ * Mission : print the alphabet, in lowercase, followed by a new line 10 times
+ * my sulotion: using _putchar to print to print each char
+ *              while loop printing letters one by one in the same line
+ *		for loop repets the while loop code 10 times
+ *
+ */
+
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ * using _putchar function again
+ *
+ * Return: (void)
+ */
+
+void print_alphabet_x10(void)
+{
+	char i, letter;
+
+	for (i = 0; i < 10; i++)
+	{
+		letter = 'a';
+
+		while (letter <= 'z')
+		{
+		_putchar(letter++);
+		}
+		_putchar('\n');
+	}
+}
