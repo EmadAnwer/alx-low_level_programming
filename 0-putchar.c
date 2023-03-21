9966a5801295303742d3834ffe2b9e15af465155
Emad Anwer <emadanwer.official@gmail.com>
2023-03-21 14:05:29 +0200

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,33 @@
+#include "_putchar.c"
+/*
+ * Task 1 in 0x02. C - Functions, nested loops
+ * Purpose : including our own header files
+ * Mission : includ _putchar.c
+ *           use _putchar.c
+ *	     print "_putchar"
+ * https://github.com/holbertonschool/_putchar.c/blob/master/_putchar.c
+ * my sulotion: print "_putchar" letter by letter
+ *              print new line at the end
+ */
+
+/**
+ * main - function is the Entry Point for this porgram
+ * prints-> "_putchar" followed by a new line.
+ * Return: value is (0)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
