9c20a9ddd7680511190eb368afd93365e310897e
Emad Anwer <emadanwer.official@gmail.com>
2023-03-21 22:07:59 +0200

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * Task 10 in 0x02. C - Functions, nested loops
+ * Purpose : adds two integers and returns the result.
+ */
+
+/**
+ * add - adds two integers and returns the result
+ * @n1: int number
+ * @n2: int number
+ * Return: (n) result
+ *
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -52,3 +52,4 @@ void times_table(void)
 		_putchar('\n');
 	}
 }
+
