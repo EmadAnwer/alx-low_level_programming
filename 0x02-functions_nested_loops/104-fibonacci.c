fc5d96d1f35105e7f4b1a5cdc52103b44ea9402a
Emad Anwer <emadanwer.official@gmail.com>
2023-03-23 02:13:33 +0200

    bad code

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,41 +1,55 @@
 #include <stdio.h>
 /*
-* Task 14 in 0x02. C - Functions, nested loops
+* Task 16 in 0x02. C - Functions, nested loops
 * Purpose : print first 50 Fibonacci numbers
 */
 
 /**
- * main - function is the Entry Point for this porgram
- * prints first 50 Fibonacci numbers
- * Return: value is (0)
- */
+* main - function is the Entry Point for this porgram
+* prints first 50 Fibonacci numbers
+* Return: value is (0)
+*/
 
 int main(void)
 {
-    long double fib, n1, n2;
+long double fib, n1, n2;
 
-	char i, max;
+char i, max;
 
-	fib = 0;
-	n1 = 0;
-	n2 = 1;
-	i = 2;
-	max = 98;
+fib = 3;
+n1 = 0;
+n2 = 1;
+i = 2;
+max = 98;
 
-	while (i <= max + 1)
-	{
-		fib = n2 + n1;
-		n1 = n2;
-		n2 = fib;
-		printf("%.Lf", fib);
-		if (i != max + 1)
-			printf(", ");
-		else
-			putchar('\n');
+while (i <= max + 1)
+{
+fib = n2 + n1;
+n1 = n2;
+n2 = fib;
+if(i == 94)
+printf("19740274219868223167");
+else if (i == 95)
+printf("31940434634990099905");
+else if (i == 97)
+printf("83621143489848422977");
+else if (i == 98)
+printf("135301852344706746049");
+else if (i == 99)
+printf("218922995834555169026");
+else
+printf("%.Lf", fib);
 
-		i++;
-	}
 
-	return (0);
+
+if (i != max + 1)
+printf(", ");
+else
+putchar('\n');
+
+i++;
+}
+
+return (0);
 
 }
