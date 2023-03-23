87c630f5433840b5a829b7374784aea8952570dc
Emad Anwer <emadanwer.official@gmail.com>
2023-03-23 17:43:43 +0200

    task2 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * Task 2 in 0x04. C - More functions, more nested loops
+ * Purpose : multiplies two integers.
+ */
+
+
+/**
+ * mul - multiplies two integers.
+ * @a: int number
+ * @b: int number
+ * Return: (a * b)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,4 +4,5 @@
 int _isdigit(int c);
 int _putchar(char c);
 int _isupper(int c);
+int mul(int a, int b);
 #endif
