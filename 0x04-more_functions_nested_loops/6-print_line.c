a23ecf18ebdfd41e53650207dba0c08ce60bc0ff
Emad Anwer <emadanwer.official@gmail.com>
2023-03-23 21:38:57 +0200

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/*
+ * Task 6 in 0x04. C - More functions, more nested loops
+ * Purpose : print '_' n time followed by a new line.
+ */
+
+
+/**
+ * print_line - print '_' n time followed by a new line.
+ * using _putchar function
+ * @n: int number
+ * Return: (void)
+ */
+
+void print_line(int n)
+{
+	if (n < 0)
+		n = n * -1;
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
