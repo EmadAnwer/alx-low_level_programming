9636e1455b0329d4a86086af1d7f2314cce5623f
Emad Anwer <emadanwer.official@gmail.com>
2023-03-24 00:39:20 +0200

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/*
+* Task 9 in 0x04. C - More functions, more nested loops
+* Purpose : The “Fizz-Buzz test” is an interview question
+*/
+
+/**
+ * main - prints the numbers from 1 to 100, followed by a new line.
+ * Return: value is (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+
+}
