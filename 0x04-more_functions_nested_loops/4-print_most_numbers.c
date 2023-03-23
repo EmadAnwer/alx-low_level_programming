19d7d6f3b8e5f403982689489892c8e3857ea6ab
Emad Anwer <emadanwer.official@gmail.com>
2023-03-23 18:00:46 +0200

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/*
+ * Task 4 in 0x02. C - Functions, nested loops
+ * Purpose : numbers, from 0 to 9, followed by a new line skip 2 4
+ * my sulotion: using _putchar and while loops printing numbers one by one
+ */
+
+
+/**
+ * print_most_numbers - numbers, from 0 to 9, followed by a new line skip 2 4
+ * using _putchar function
+ *
+ * Return: (void)
+ */
+
+void print_most_numbers(void)
+{
+	char n = '0';
+
+	while (n <= '9')
+	{
+		if (n != '2' && n != '4')
+			_putchar(n++);
+	}
+	_putchar('\n');
+}
