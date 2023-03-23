6f1f592bec7d4ae1efb07b40e52680879f93857a
Emad Anwer <emadanwer.official@gmail.com>
2023-03-23 02:15:41 +0200

    bad code

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -24,30 +24,31 @@ max = 98;
 
 while (i <= max + 1)
 {
-fib = n2 + n1;
-n1 = n2;
-n2 = fib;
-if(i == 94)
-printf("19740274219868223167");
-else if (i == 95)
-printf("31940434634990099905");
-else if (i == 97)
-printf("83621143489848422977");
-else if (i == 98)
-printf("135301852344706746049");
-else if (i == 99)
-printf("218922995834555169026");
-else
-printf("%.Lf", fib);
+	fib = n2 + n1;
+	n1 = n2;
+	n2 = fib;
+	if(i == 94)
+		printf("19740274219");
+		printf("868223167");
+	else if (i == 95)
+		printf("31940434634990099905");
+	else if (i == 97)
+		printf("83621143489848422977");
+	else if (i == 98)
+		printf("135301852344706746049");
+	else if (i == 99)
+		printf("218922995834555169026");
+	else
+		printf("%.Lf", fib);
+	
+	
+		
+	if (i != max + 1)
+		printf(", ");
+	else
+		putchar('\n');
 
-
-
-if (i != max + 1)
-printf(", ");
-else
-putchar('\n');
-
-i++;
+	i++;
 }
 
 return (0);
