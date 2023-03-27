c47f2d9eb87995ae549c83024de5dd18a2f6d890
Emad Anwer <emadanwer.official@gmail.com>
2023-03-27 19:34:12 +0200

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+/*
+ * 0x05. C - prints a string, in reverse
+ * task 5
+ * Purpose : reverses a string.
+ */
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverses a string with refrance
+ * @s: char pointer
+ * Return: the length *s
+ */
+void rev_string(char *s)
+{
+	int len, i;
+	char swap;
+
+	len = strlen(s) - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		swap = s[i];
+		s[i] = s[len - i];
+		s[len - i] = swap;
+	}
+}
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 /**
  * main - check the code
  *
@@ -8,10 +8,25 @@
  */
 int main(void)
 {
-    char *str;
+    char s[10] = "My School";
+	char p[11] = "My SchoolE";
+	char xp[1] = "";
+    printf("%s\n", s);
+    rev_string(s);
+    printf("%s\n", s);
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    print_rev(str);
-    return (0);
+
+
     
+
+    printf("%s\n", p);
+    rev_string(p);
+    printf("%s\n", p);
+
+
+	 printf("%s\n", xp);
+    rev_string(xp);
+    printf("%s\n", xp);
+    return (0);
+
 }
