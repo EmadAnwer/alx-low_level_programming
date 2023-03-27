05bca7845adb3e01c9b37db175390dee325d8c82
Emad Anwer <emadanwer.official@gmail.com>
2023-03-27 13:01:06 +0200

    task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/*
+ * 0x05. C - Pointers, arrays and strings
+ * task 0. 98 Battery st.
+ * Purpose : updates the value of pointer
+ */
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of n pointer
+ * @n: intger pointer
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+0x05. C - Pointers, arrays and strings
+
+Concepts
+
+    Pointers and arrays
+    Data Structures
