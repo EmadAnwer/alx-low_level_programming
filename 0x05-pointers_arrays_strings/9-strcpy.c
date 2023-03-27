9bed6133c207e687b5379a3723e06f65d13fd685
Emad Anwer <emadanwer.official@gmail.com>
2023-03-27 22:41:43 +0200

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,23 +1,23 @@
 /*
  * 0x05. C - Pointers, arrays and strings
  * task 9
- * Purpose : returns the length of a string.
+ * Purpose : copies the string pointed
  */
 #include "main.h"
 /**
- * _strlen - returns the length of a string.
+ * _strcpy - copies the string pointed
  * @dest: char pointer
  * @src: char pointer
- * Return: char pinter 
+ * Return: char pinter
  */
 char *_strcpy(char *dest, char *src)
 {
-    int i ;
-    for (i = 0; src[i] != '\0'; i++)
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
 	dest[i] = '\0';
-	
-    return (dest);
+	return (dest);
 }
