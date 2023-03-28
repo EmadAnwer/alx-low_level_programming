177fc95d6e723bfa2355bbf19dda4d211d8f5fa6
Emad Anwer <emadanwer.official@gmail.com>
2023-03-28 03:21:40 +0200

    1 adv

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+/*
+ * 0x05. C - Pointers, arrays and strings
+ * task 10 advanced
+ * Purpose : convert a string to an integer.
+ */
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - convert a string to an integer.
+ * @s: char pointer
+ * Return: char pinter
+ */
+int _atoi(char *s)
+{
+	int n, i;
+	char negative, positive;
+
+	positive = 0;
+	negative = 0;
+	i = 0;
+	n = 0;
+	while (s[i] != '\0')
+	{
+		if (!((s[i] >= '0') && (s[i] <= '9')) && n > 0)
+		{
+			break;
+		}
+		if (((s[i] >= '0') && (s[i] <= '9')))
+		{
+			n = (n * 10) + (s[i] - '0');
+		}
+		else if (s[i] == '-')
+			negative++;
+		else if (s[i] == '+')
+			positive++;
+
+		i++;
+	}
+	if (positive < negative)
+		n = n * -1;
+return (n);
+}
