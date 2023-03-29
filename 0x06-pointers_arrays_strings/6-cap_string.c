e4f0ee2a4755173b7de073b1115fa685c5aaefe3
Emad Anwer <emadanwer.official@gmail.com>
2023-03-29 20:27:01 +0200

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+/*
+ * 0x06. C - More pointers, arrays and strings
+ * task 6
+ */
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @src: string refrance
+ * Return: *src "all words of a string capital"
+ */
+char *cap_string(char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		if ((src[i] == ' ')
+		|| (src[i] == '\t')
+		|| (src[i] == '\n')
+		|| (src[i] == ',')
+		|| (src[i] == ';')
+		|| (src[i] == '.')
+		|| (src[i] == '!')
+		|| (src[i] == '?')
+		|| (src[i] == '"')
+		|| (src[i] == '(')
+		|| (src[i] == ')')
+		|| (src[i] == '{')
+		|| (src[i] == '}'))
+		{
+			if ((src[i + 1] >= 'a') && (src[i + 1] <= 'z'))
+			{
+				src[i + 1] = src[i + 1] - 32;
+			}
+		}
+		i++;
+	}
+	return (src);
+}
