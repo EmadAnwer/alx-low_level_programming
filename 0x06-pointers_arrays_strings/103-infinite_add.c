7ef89f0cf9cd8665c982899c7e777e51862ab52f
Emad Anwer <emadanwer.official@gmail.com>
2023-03-30 02:01:54 +0200

    will fix soon

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,18 @@
+/*
+ * 0x06. C - More pointers, arrays and strings
+ * task 6
+ */
+#include "main.h"
+/**
+ * infinite_add - capitalizes all words of a string
+ * @n1: string refrance
+ * @n2: string refrance
+ * @r: string refrance
+ * @size_r: string refrance
+ * Return: *src "all words of a string capital"
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	n1[0] = 5;
+	return (n2);
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,14 @@
+/*
+ * 0x06. C - More pointers, arrays and strings
+ * task 4
+ */
+#include "main.h"
+/**
+ * print_buffer - reverse array elments
+ * @b: size of an array
+ * @size: array refrance
+ */
+void print_buffer(char *b, int size)
+{
+
+}
