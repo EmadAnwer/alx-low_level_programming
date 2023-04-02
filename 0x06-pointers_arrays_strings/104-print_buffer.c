afc44c279a0522d23b94fcf65d5e32601f23e394
Emad Anwer <emadanwer.official@gmail.com>
2023-04-02 17:47:14 +0200

    104 test

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -3,6 +3,7 @@
  * task 4
  */
 #include "main.h"
+#include "stdio.h"
 /**
  * print_buffer - reverse array elments
  * @b: size of an array
@@ -10,5 +11,34 @@
  */
 void print_buffer(char *b, int size)
 {
+	int i, z;
 
+	z = 0;
+	while (z < size)
+	{
+		printf("%08x: ", z);
+		for (i = 0; i < 10 && i < size; i++)
+		{
+			if (z + i >= size)
+			{
+				printf("     ");
+				i++;
+			}
+			else
+			{
+				printf("%02x", b[z + i]);
+				i++;
+				printf("%02x ", b[z + i]);
+			}
+		}
+		for (i = 0; i < 10 && z + i < size; i++)
+		{
+			if (b[z + i] < 31)
+				printf(".");
+			else
+				printf("%c", b[z + i]);
+		}
+		printf("\n");
+		z += 10;
+	}
 }
