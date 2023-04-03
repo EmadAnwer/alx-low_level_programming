3cd72ebd44b2cce0e2d7cc094e7a38c3dc247034
Emad Anwer <emadanwer.official@gmail.com>
2023-04-03 14:12:16 +0200

    task 0 - fix betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,17 +5,19 @@
 #include "main.h"
 #include "stdio.h"
 /**
- * print_buffer -  fills memory buffer with a constant byte.
+ * _memset -  fills memory buffer with a constant byte.
  * @s: array refrance
  * @b: constant byte
- * @b: buffer size
+ * @n: buffer size
+ * Return: s array refrance
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
+
 	for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
-    return s;
+	{
+		s[i] = b;
+	}
+	return (s);
 }
