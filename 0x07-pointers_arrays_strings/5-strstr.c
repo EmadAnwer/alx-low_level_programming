3479d3ad8925ce1011a6a242e707ea93cdc88c8d
Emad Anwer <emadanwer.official@gmail.com>
2023-04-04 00:55:05 +0200

    t

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,7 +15,7 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	for (i = 0; needle[i] != '\0' && needle[0] != '\0'; i++)
 	{
 		for (j = 0; haystack[j] != '\0'; j++)
 		{
