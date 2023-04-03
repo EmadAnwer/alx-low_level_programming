51e2011586600dbdb32abe479941a398937f9b24
Emad Anwer <emadanwer.official@gmail.com>
2023-04-04 00:13:32 +0200

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+/*
+ * 0x07. C - Even more pointers, arrays and strings
+ * task 4
+ */
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strpbrk - returns the number of characters in the initial segment
+ * @s: array refrance
+ * @accept: array refrance
+ * Return: array refrance
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, sp;
+
+	sp = 1000000000;
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (s[j] == accept[i] && j < sp)
+			{
+				sp = j;
+			}
+		}
+	}
+	return (s + sp);
+}
