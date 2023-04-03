9ca23f233ec6ead3f2dedd77c9068693f9779461
Emad Anwer <emadanwer.official@gmail.com>
2023-04-04 00:47:45 +0200

    task5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+/*
+ * 0x07. C - Even more pointers, arrays and strings
+ * task 5
+ */
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strpbrk - returns the number of characters in the initial segment
+ * @haystack: array refrance
+ * @needle: array refrance
+ * Return: array refrance
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		for (j = 0; haystack[j] != '\0'; j++)
+		{
+			if (haystack[j] == needle[i])
+				return (haystack + j);
+		}
+	}
+
+	return (NULL);
+
+}
