05c4f9dcd64d3acb7759ae01da9840f6912e23b2
Emad Anwer <emadanwer.official@gmail.com>
2023-04-05 23:35:12 +0200

    bad task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+/*
+ * 0x08. C - Recursion
+ * task 6
+ */
+/**
+ * sqrtHelper - x pow y
+ * @n: int
+ * @factor: int factor
+ * Return: sqrt int
+ */
+int check_prime(int factor, int n)
+{
+	/*bad algorithm*/
+	if (factor == n)
+		return (1);
+	if (n % factor == 0)
+		return (0);
+	return (check_prime(factor + 1, n));
+}
+/**
+ * is_prime_number - prime number cheaker
+ * @n: int
+ * Return: returns 1 if prime
+ */
+int is_prime_number(int n)
+{
+	if (n == 1 || n == -1)
+		return (0);
+	return (check_prime(2, n));
+}
