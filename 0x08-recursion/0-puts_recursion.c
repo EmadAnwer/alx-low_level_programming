9154a8f20b0c9231b8ad72e55f81018c43545fd8
Emad Anwer <emadanwer.official@gmail.com>
2023-04-05 15:53:10 +0200

    fix betty

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,18 +4,18 @@
  */
 #include "main.h"
 /**
- * _puts_recursion - prints a string recursively 
+ * _puts_recursion - prints a string recursively
  * @s: array refrance "string"
  */
 void _puts_recursion(char *s)
 {
-	
-	if(*s != '\0')
+
+	if (*s != '\0')
 	{
 		_putchar(s[0]);
 		_puts_recursion(s + 1);
 	}
 	else
 		_putchar('\n');
-	
+
 }
