9279acded0f1504fe976101a3cccfb8bb719137f
Emad Anwer <emadanwer.official@gmail.com>
2023-04-05 23:07:09 +0200

    task 5 bad algorithm

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+/*
+ * 0x08. C - Recursion
+ * task 5
+ */
+#include "main.h"
+
+/**
+ * sqrtHelper - x pow y
+ * @v: int
+ * @n: int power
+ * Return: sqrt int
+ */
+int sqrtHelper(int v, int n)
+{
+	/*bad algorithm*/
+	if (v * v == n)
+		return (v);
+	if (v <= 0)
+		return (-1);
+	return (sqrtHelper(v - 1, n));
+}
+/**
+ * _sqrt_recursion - x pow y
+ * @n: int
+ * Return: sqr using sqrtHelper
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrtHelper(n, n));
+}
