8a56ec55695f29d9878e36ba79e2690fdb0892d5
Emad Anwer <emadanwer.official@gmail.com>
2023-04-06 18:14:08 +0200

    fix bug

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,6 +2,7 @@
  * 0x08. C - Recursion
  * task 8 adv "not finished yet"
  */
+#include <stdio.h>
 /**
  * wildcmp - compares two strings and returns 1
  * @s1: string refrance
@@ -15,14 +16,16 @@ int wildcmp(char *s1, char *s2)
 
 	if (*s1 == *s2)
 	{
+		
 		return (wildcmp(s1 + 1, s2 + 1));
+		
 	}
-	else if (*s2 == '*')
+	
+	if (*s2 == '*')
 	{
-		if (*s2 + 1 == '*')
+		if (*(s2 + 1) == '*')
 			return (wildcmp(s1, s2 + 1));
-
-		if ((wildcmp(s1, s2 + 1)) || (wildcmp(s1 + 1, s2)))
+		if ((wildcmp(s1 + 1, s2)) || (wildcmp(s1, s2 + 1)))
 			return (1);
 	}
 	return (0);
