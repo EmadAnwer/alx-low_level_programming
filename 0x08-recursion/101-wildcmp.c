9df1a72ba15c5624d66aad72cf7d47248b2aa3fe
Emad Anwer <emadanwer.official@gmail.com>
2023-04-06 17:21:48 +0200

    last <3

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,18 +10,20 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if ((*s1 == '\0') || (*s2 == '\0'))
+	if ((*s1 == '\0') && (*s2 == '\0'))
 		return (1);
+
 	if (*s1 == *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
-	if (*s2 == '*')
+	else if (*s2 == '*')
 	{
-		if (*s1 != ' ')
-			return (wildcmp(s1 + 1, s2));
-		else
-			return (wildcmp(s1 + 1, s2 + 1));
+		if (*s2 + 1 == '*')
+			return (wildcmp(s1, s2 + 1));
+
+		if ((wildcmp(s1, s2 + 1)) || (wildcmp(s1 + 1, s2)))
+			return (1);
 	}
 	return (0);
 }
