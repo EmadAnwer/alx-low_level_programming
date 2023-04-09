29a721a516520a31c1cba2e0770a521264e8fb58
Emad Anwer <emadanwer.official@gmail.com>
2023-04-09 17:12:07 +0200

    task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * Task 5 in 0x0A. C - argc, argv
+ * Purpose : minimum number of coins to make change for an amount of money.
+ */
+
+/**
+ * main - minimum number of coins to make change for an amount of money.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: (0) success or (1) faild
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, total, i;
+	int changes[4] = {25,10,2,1};
+
+	a = 0;
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	total = atoi(argv[1]);
+	if(total <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 0; i < 4; i++)
+	{
+		a = a + (total / changes[i]);
+		total = total % changes[i] ;
+		if(total == 0)
+			break;
+	}
+	printf("%d\n", a);
+	return (0);
+}
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*
- * Task 3 in 0x0A. C - argc, argv
+ * Task 4 in 0x0A. C - argc, argv
  * Purpose : adds positive numbers.
  */
 
