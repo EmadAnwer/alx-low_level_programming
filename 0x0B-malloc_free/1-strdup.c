0de60e00c25aefe09ce30fdb2af1dbc2616c60a4
Emad Anwer <emadanwer.official@gmail.com>
2023-04-11 17:06:52 +0200

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+/*
+ * 0x0B. C - malloc, free
+ * task 0
+ */
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - creates an array of chars
+ * @str: array size
+ * Return: array refrance or NULL
+ */
+char *_strdup(char *str)
+{
+	int len;
+	char *arr;
+
+	len = strlen(str);
+	arr = malloc(sizeof(char) * len);
+	if (arr == NULL)
+		return (NULL);
+	while (len--)
+		arr[len] = str[len];
+	return (arr);
+}
