7e391340ba5a2b18090ffbaef48176d60168b27d
Emad Anwer <emadanwer.official@gmail.com>
2023-04-11 22:05:52 +0200

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
-		if (grid == NULL)
+		if (grid[i] == NULL)
 			return (NULL);
 	}
 	return (grid);
