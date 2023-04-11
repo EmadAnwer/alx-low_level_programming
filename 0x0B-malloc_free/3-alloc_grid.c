ec40f861e1d602c1093bce345924e5cdc3133b77
Emad Anwer <emadanwer.official@gmail.com>
2023-04-11 22:04:43 +0200

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+/*
+ * 0x0B. C - malloc, free
+ * task 0
+ */
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *alloc_grid - creates an array of chars
+ * @width: array width
+ * @height: array height
+ * Return: 2D int array refrance  or NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid, i;
+
+	if ((width <= 0) || (height <= 0))
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid == NULL)
+			return (NULL);
+	}
+	return (grid);
+}
