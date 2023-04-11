c2fbed54fc812fb2a9aac528e1df15111a46e665
Emad Anwer <emadanwer.official@gmail.com>
2023-04-11 19:51:38 +0200

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,16 +16,15 @@ char *str_concat(char *s1, char *s2)
 	int len, i, j;
 	char *arr;
 
-	if ((s1 == NULL) && (s2 == NULL))
-		return (NULL);
 	if (s1 == NULL)
 		len = strlen(s2) + 1;
 	else if (s2 == NULL)
 		len = strlen(s1) + 1;
 	else
-		len = (sizeof(s1) + (sizeof(s1))) + 1;
+		len = (strlen(s1) + (strlen(s1)));
 	arr = malloc(len);
-
+	if (arr == NULL)
+		return (NULL);
 	if (s1 == NULL)
 		for (i = 0; i < len; i++)
 			arr[i] = s2[i];
