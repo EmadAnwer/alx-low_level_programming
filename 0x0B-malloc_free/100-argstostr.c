68a63cc593e955f74262aac9ab9a824233f0b803
Emad Anwer <emadanwer.official@gmail.com>
2023-04-12 00:32:04 +0200

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+/*
+ * 0x0B. C - malloc, free
+ * task 2
+ */
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * argstostr - concatenates argument vector
+ * @ac: height
+ * @av: 2D array refrance
+ * Return: array refrance full string
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, len, j;
+	char *arr, *tar;
+
+	len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		len = len + strlen(av[i]) + 1;
+	}
+
+	arr = malloc(len);
+	if (arr == NULL)
+		return (NULL);
+
+	tar = arr;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j <= (int)strlen(av[i]); j++)
+		{
+			if (av[i][j] == '\0')
+				*arr = '\n';
+			else
+				*arr = av[i][j];
+			arr++;
+		}
+	}
+	*arr = '\0';
+	return (tar);
+}
