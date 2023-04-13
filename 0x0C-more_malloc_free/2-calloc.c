ce3516805aa546d4e9ae166c15961fae4383a5ae
Emad Anwer <emadanwer.official@gmail.com>
2023-04-13 15:50:48 +0200

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+/*
+ * 0x0C. C - More malloc, free
+ * task 0
+ */
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory using calloc.
+ * @nmemb: This is the number of elements
+ * @size: This is the size of elements.
+ * Return: array refrance or NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = calloc(nmemb, size);
+	if (p == NULL)
+		return (NULL);
+	return (p);
+}
