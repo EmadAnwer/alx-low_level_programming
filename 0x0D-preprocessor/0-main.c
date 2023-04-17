d27d98e9a18d480522957c476482363844d36597
Emad Anwer <emadanwer.official@gmail.com>
2023-04-17 13:35:52 +0200

    fix

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int i;
+
+    i = ABS(-2 - 5) ;
+    
+    printf("%d\n", i);
+    return (0);
+}
