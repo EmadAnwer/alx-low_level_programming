28c09bb900a01acc3eefa40ef3c35d3e0afc6660
Emad Anwer <emadanwer.official@gmail.com>
2023-04-19 12:15:18 +0200

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+/*
+ * 0x0F. C - Function pointers
+ * task 0
+ */
+#include "function_pointers.h"
+#include <string.h>
+/**
+ * print_name - prints a name.
+ * @name: char pointer to name
+ * @f: funtcion pointer
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
