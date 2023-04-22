fdf4c860076a39afceaf016a72f1a79e9eb73627
Emad Anwer <emadanwer.official@gmail.com>
2023-04-22 02:32:54 +0200

    dd

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < (int)n; i++)
 	{
-		s = va_arg(args, int);
+		s = va_arg(args, char*);
 		if (s != NULL)
 			printf("%s", s);
 		else
