65712be22237340914ad359a1ee3661775748d78
Emad Anwer <emadanwer.official@gmail.com>
2023-04-27 17:32:38 +0200

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,41 @@
+/*
+ * 0x12. C - Singly linked lists
+ * task 2
+ * my solution allocate a new list_t then duplicate the string
+ * at the end change the head reference to the last created node "list_t l"
+ */
+#include "lists.h"
+/**
+ * head - pointer to a head pointer
+ * @str: string reference
+ * Return: list size
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	char *s_cpy;
+	list_t *l;
+
+	l = malloc(sizeof(list_t));
+	if (l == NULL)
+		return (NULL);
+	
+	if(str)
+	{
+		s_cpy = strdup(str);
+		if(s_cpy == NULL)
+		{
+			free(l);
+			return (NULL);
+		}
+		l->str = s_cpy;
+		l->len = strlen(s_cpy);
+	}
+	else
+	{
+		l->str = NULL;
+		l->len = 0;
+	}
+	l->next = (*head);
+	*head = l;
+	return (l);
+}
