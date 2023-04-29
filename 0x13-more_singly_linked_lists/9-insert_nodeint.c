92b21952989c094c8873b052acf7d3d02eed03a6
Emad Anwer <emadanwer.official@gmail.com>
2023-04-29 16:01:17 +0200

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,24 +1,45 @@
 /*
  * 0x13. C - More singly linked lists
- * task 6
+ * task 9
  */
 #include "lists.h"
 /**
- * sum_listint - deletes the head node of a listint_t linked list
+ * insert_nodeint_at_index - deletes the head node of a listint_t linked list
  * @head: pointer to a head pointer
+ * @idx: index
+ * @n: number
  * Return: the head node’s data (n) or 0
  */
-int sum_listint(listint_t *head)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	int sum;
+	listint_t *next, *new;
 
 	if (head == NULL)
-		return (0);
-	sum = 0;
-	while (head)
+		return (NULL);
+	if (idx == 0)
 	{
-		sum = sum + head->n;
-		head = head->next;
+		new = malloc(sizeof(listint_t));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		new->next = (*head)->next;
+		(*head) = new;
+		return (new);
 	}
-	return (sum);
+	next = *head;
+	while (idx != 1)
+	{
+		next = next->next;
+		if ((next == NULL) && (idx != 1))
+			return (NULL);
+		idx--;
+	}
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = next->next;
+	next->next = new;
+
+	return (new);
 }
