54d9a0cec8416fbce8ef4aa7aaa359785b7c7147
Emad Anwer <emadanwer.official@gmail.com>
2023-05-04 22:31:31 +0200

    test

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,46 @@
+/*
+ * 0x14. C - Bit manipulation
+ * task 4
+ */
+#include "main.h"
+/*prototypes*/
+unsigned int _pow(int base, int pow);
+/**
+ * clear_bit -  sets the value of a bit to 0 at a given index.
+ * @n: binary number array pointer.
+ * @index: binary number array pointer.
+ * Return: ndex 1 or -1 if an error occured
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int p;
+	if (index >= 32)
+		return (-1);
+	p = _pow(2, index);
+	if(p > *n)
+		*n = 0;
+	else 
+		*n = *n - _pow(2, index);
+	return (1);
+}
+/**
+ * _pow - calculate power
+ * @base: base
+ * @pow: power
+ * Return: result
+ */
+unsigned int _pow(int base, int pow)
+{
+	unsigned int result;
+
+	if (pow == 0)
+		return (1);
+
+	if (pow == 1)
+		return (base);
+	result = base;
+	while (--pow)
+		result *= base;
+	return (result);
+}
