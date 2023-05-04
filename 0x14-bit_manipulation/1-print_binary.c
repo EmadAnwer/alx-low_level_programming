02cf1633116ed61b39b66a752d04c07801318aea
Emad Anwer <emadanwer.official@gmail.com>
2023-05-04 20:02:04 +0200

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+/*
+ * 0x14. C - Bit manipulation
+ * task 1
+ */
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: binary number array pointer.
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar('0'+ (n & 1));
+}
