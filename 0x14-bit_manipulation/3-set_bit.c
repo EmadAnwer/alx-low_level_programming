0a59c2563067527bf76ee6dcd06f8b5563eb478e
Emad Anwer <emadanwer.official@gmail.com>
2023-05-04 22:25:15 +0200

    test

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,41 @@
+/*
+ * 0x14. C - Bit manipulation
+ * task 1
+ */
+#include "main.h"
+/*prototypes*/
+unsigned int _pow(int base, int pow);
+/**
+ * set_bit -  sets the value of a bit to 1 at a given index.
+ * @n: binary number array pointer.
+ * @index: binary number array pointer.
+ * Return: ndex 1 or -1 if an error occured
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= 32)
+		return (-1);
+	*n = *n + _pow(2, index); 
+	return (1);
+}
+/**
+ * _pow - calculate power
+ * @base: base
+ * @pow: power
+ * Return: result
+ */
+unsigned int _pow(int base, int pow)
+{
+	unsigned int result;
+
+	if (pow == 0)
+		return (1);
+
+	if (pow == 1)
+		return (base);
+	result = base;
+	while (--pow)
+		result *= base;
+	return (result);
+}
