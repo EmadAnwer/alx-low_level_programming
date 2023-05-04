47e77bf2780be2a25d24ce6c9d18446b6289c1cf
Emad Anwer <emadanwer.official@gmail.com>
2023-05-04 17:17:05 +0200

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,15 +1,15 @@
 /*
- * 0x0C. C - More malloc, free
- * task 2
+ * 0x14. C - Bit manipulation
+ * task 0
  */
 #include "main.h"
 #include <string.h>
+/*prototypes*/
 unsigned int _pow(int base, int pow);
 /**
- * _calloc - allocates memory using calloc.
- * @nmemb: This is the number of elements
- * @size: This is the size of elements.
- * Return: array refrance or NULL
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: binary number array pointer.
+ * Return: converted number, or 0.
  */
 unsigned int binary_to_uint(const char *b)
 {
@@ -17,29 +17,33 @@ unsigned int binary_to_uint(const char *b)
 
 	dismal = 0;
 	i = 0;
-	if(!b)
+	if (!b)
 		return (0);
 
 	len = strlen(b);
 	while (b[i])
 	{
-		if(!(b[i] == '0' ||  b[i] == '1'))
+		if (!(b[i] == '0' ||  b[i] == '1'))
 			return (0);
 		dismal = dismal + (b[i] - '0') * _pow(2, len - i - 1);
 		i++;
 	}
-	
 	return (dismal);
 }
-
+/**
+ * _pow - calculate power
+ * @base: base
+ * @pow: power
+ * Return: result
+ */
 unsigned int _pow(int base, int pow)
 {
 	unsigned int result;
 
-	if(pow == 0)
+	if (pow == 0)
 		return (1);
 
-	if(pow == 1)
+	if (pow == 1)
 		return (base);
 	result = base;
 	while (--pow)
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -1,5 +1,6 @@
-0x0C. C - More malloc, free
+0x14. C - Bit manipulation
+
 
 General
-How to use the exit function
-What are the functions calloc and realloc from the standard library and how to use them
+	Look for the right source of information without too much help
+	How to manipulate bits and use bitwise operators
