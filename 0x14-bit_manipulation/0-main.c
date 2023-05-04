31ae1979ecca0877d568a00ba0ceb752ef7a1049
Emad Anwer <emadanwer.official@gmail.com>
2023-05-04 20:59:50 +0200

    test

--- 0x14-bit_manipulation/0-main.c
+++ 0x14-bit_manipulation/0-main.c
@@ -14,7 +14,7 @@ int main(void)
     printf("%d\n", n);
     n = get_bit(98, 1);
     printf("%d\n", n);
-    n = get_bit(1024, 0);
+    n = get_bit(0, 0);
     printf("%d\n", n);
     return (0);
 }
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,5 +10,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	return(n >> index & 1);
+	if(n >> index > 0)
+		return(n >> index & 1);
+	return(-1);
 }
