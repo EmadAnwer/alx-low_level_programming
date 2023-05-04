67a6ffe6d6870c486f697a790fb2d63aa8d1f249
Emad Anwer <emadanwer.official@gmail.com>
2023-05-04 23:15:38 +0200

    done

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+/*
+ * 0x14. C - Bit manipulation
+ * task 5
+ */
+#include "main.h"
+/**
+ * flip_bits -  sets the value of a bit to 0 at a given index.
+ * @n: number
+ * @m: number
+ * Return: number of bits you would need to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int p, f;
+
+	p = 32;
+	f = 0;
+	while (p--)
+	{
+		if ((n & 1) != (m & 1))
+			f++;
+		n >>= 1;
+		m >>= 1;
+	}
+	return (f);
+}
