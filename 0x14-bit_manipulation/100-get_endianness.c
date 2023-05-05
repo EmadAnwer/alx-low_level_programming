5c3a4244e5708f507facb7158ad26a15a5fe6bc9
Emad Anwer <emadanwer.official@gmail.com>
2023-05-05 03:42:25 +0200

    test

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+/*
+ * 0x14. C - Bit manipulation
+ * task adv 1
+ */
+#include "main.h"
+/**
+ * get_endianness - checks the endianness.
+ * Return: 0 or 1
+ */
+
+int get_endianness(void)
+{
+	unsigned int x = 1;
+	char *c = (char*) &x;
+	return (int)*c;
+}
