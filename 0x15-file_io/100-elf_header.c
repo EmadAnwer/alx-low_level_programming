d29d736380040c61a117992cb5174399b5db31b6
Emad Anwer <emadanwer.official@gmail.com>
2023-05-09 01:14:23 +0200

    tomorrow :(

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,22 @@
+#include <elf.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @elfFile: elfFile name
+ * Return: 1 on success, -1.
+ */
+void read_elf_header(const char* elfFile) {
+
+}
+/**
+ * main - check the code
+ * @ac: arguments count
+ * @av: arguments vactor
+ * Return: 0 success Otherwise failed
+ */
+int main(int ac, char **av)
+{
+	
+	exit(98);
+}
