57bf6b65d63aff06c9e873d8a37d372de7a01df7
Emad Anwer <emadanwer.official@gmail.com>
2023-05-09 01:19:55 +0200

    todo

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,14 +1,12 @@
 #include <elf.h>
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * append_text_to_file - appends text at the end of a file.
- * @elfFile: elfFile name
- * Return: 1 on success, -1.
- */
-void read_elf_header(const char* elfFile) {
-
-}
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
 /**
  * main - check the code
  * @ac: arguments count
@@ -17,6 +15,18 @@ void read_elf_header(const char* elfFile) {
  */
 int main(int ac, char **av)
 {
-	
-	exit(98);
+	int fd;
+	if (ac != 2)
+	{
+		write(STDERR_FILENO, "Usage: elf_header elf_filename\n", 31);
+		exit(98);
+	}
+
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+	{
+		write(STDERR_FILENO, "Error: Can't read from file\n", 28);
+		exit(98);
+	}
+	return (0);
 }
