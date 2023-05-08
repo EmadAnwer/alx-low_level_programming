1401a6466259b780b421c162d155ab82171fdc8f
Emad Anwer <emadanwer.official@gmail.com>
2023-05-08 19:47:24 +0200

    test

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+/*
+ * 0x14. C - Bit manipulation
+ * task 0
+ */
+#include "main.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * read_textfile - eads a text file and prints it to the POSIX standard output.
+ * @filename: filename
+ * @letters: number of letters it should read and print
+ * Return: converted number, or 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_descriptor;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+	file_descriptor = open(filename, O_RDONLY);
+	if(file_descriptor == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+
+	read(file_descriptor, buf, letters);
+	if((size_t) write(STDOUT_FILENO, buf, letters) != letters)
+		return (0);
+	close(file_descriptor);
+	free(buf);
+	return (letters);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif /* MAIN_H */
