4af8fef84cc4d30983f53b9a05b9ff703c395b14
Emad Anwer <emadanwer.official@gmail.com>
2023-05-08 20:59:56 +0200

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+/*
+ * 0x15. C - File I/O
+ * task 0
+ */
+#include "main.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * create_file - function that creates a file.
+ * @filename: filename
+ * @text_content: text content
+ * Return: 1 on success, -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor;
+
+
+	if (filename == NULL)
+		return (-1);
+
+	file_descriptor = open(filename, O_WRONLY | O_TRUNC);
+	if (file_descriptor == -1)
+		file_descriptor = open(filename, O_WRONLY | O_CREAT, 0600);
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (text_content != NULL)
+		if (write(file_descriptor, text_content, strlen(text_content)) == 0)
+			return (-1);
+	close(file_descriptor);
+
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,4 +5,5 @@
 #include <stdio.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif /* MAIN_H */
