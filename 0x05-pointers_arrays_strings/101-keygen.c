4646a06dfde24124bbb365e541cd5c69b08e244c
Emad Anwer <emadanwer.official@gmail.com>
2023-06-25 00:56:47 +0200

    rt

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,19 +1,4 @@
 #include <stdio.h>
-
-int checksum(char *data)
-{
-	int sum = 0;
-	int i = 0;
-
-	while (data[i] != '\0')
-	{
-		sum += (int)data[i];
-		i++;
-	}
-
-	return sum;
-}
-
 int main()
 {
 
