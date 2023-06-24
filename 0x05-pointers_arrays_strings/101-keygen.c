fed367b3f7cd33390966481400339aad33dcb229
Emad Anwer <emadanwer.official@gmail.com>
2023-06-25 01:02:22 +0200

    rt

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 int main()
 {
-	srand((unsigned int)time(NULL));
-	printf("zzzzzzzzzzzzzzzzzzzzzzX");
+	puts("zzzzzzzzzzzzzzzzzzzzzzX");
 
 	return (0);
 }
