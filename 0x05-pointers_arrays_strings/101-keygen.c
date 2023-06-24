35a9db4bd07584787693a1cffc82cd1340450ac8
Emad Anwer <emadanwer.official@gmail.com>
2023-06-25 00:55:51 +0200

    test

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,17 +1,23 @@
-#include "main.h"
 #include <stdio.h>
-#include<stdlib.h>
-#include<time.h>
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+
+int checksum(char *data)
 {
-	
-	srand(time(0));
-   	printf("%s","he quick brown fox jumps over the lazy dog. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac mauris sed metus sagittis lacinia vitae vitae turpis. Donec eu metus nunc. Nam in lorem augue. Sed dignissim faucibus mi eu commodo. Praesent semper eros non magna elementum iaculis. Curabitur vestibulum augue et ligula pretium, quis commodo mauris fringilla. Fusce id tellus vel mauris ultrices maximus. Aliquam interdum mauris eget mi vulputate, eget dapibus ipsum euismod. Donec pulvinar ex ac justo finibus fermentum. Pellentesque malesuada vitae nunc ac dapibus. Etiam euismod euismod nibh vel elementum. Nam malesuada suscipit quam, at suscipit libero faucibus nec. Sed vel rutrum nisl.");
-	
-	return (0);
+	int sum = 0;
+	int i = 0;
+
+	while (data[i] != '\0')
+	{
+		sum += (int)data[i];
+		i++;
+	}
+
+	return sum;
+}
+
+int main()
+{
+
+	printf("zzzzzzzzzzzzzzzzzzzzzzX");
+
+	return 0;
 }
