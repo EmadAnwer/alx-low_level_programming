8ac676d0bfdb1d1ffdee84cafc92d28da69a9216
Emad Anwer <emadanwer.official@gmail.com>
2023-06-25 01:17:42 +0200

    test

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,7 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*Function to shuffle an array*/
+void shuffleArray(char *array[], int size)
+{
+	int i;
+	srand(time(NULL));
+	if (size > 1)
+	{
+		for (i = size - 1; i > 0; --i)
+		{
+			int j = rand() % (i + 1);
+			char *temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+	}
+}
+
 int main()
 {
-	puts("zzzzzzzzzzzzzzzzzzzzzzX");
+	int numStrings;
+	// Array of strings
+	const char *strings[] = {
+		"zzzzzzzzzzzzzzzzzzzzzzX",
+		"zzzzzzzzzzzzzzzzzzzzzXz",
+		"zzzzzzzzzzzzzzzzzzzzXzz",
+		"zzzzzzzzzzzzzzzzzzzXzzz",
+		"zzzzzzzzzzzzzzzzzzXzzzz",
+		"zzzzzzzzzzzzzzzzzXzzzzz",
+		"zzzzzzzzzzzzzzzzXzzzzzz",
+		"zzzzzzzzzzzzzzzXzzzzzzz",
+		"zzzzzzzzzzzzzzXzzzzzzzz",
+		"zzzzzzzzzzzzzXzzzzzzzzz",
+		"zzzzzzzzzzzzXzzzzzzzzzz",
+		"zzzzzzzzzzzXzzzzzzzzzzz",
+		"zzzzzzzzzzXzzzzzzzzzzzz",
+		"zzzzzzzzzXzzzzzzzzzzzzz",
+		"zzzzzzzzXzzzzzzzzzzzzzz",
+		"zzzzzzzXzzzzzzzzzzzzzzz",
+		"zzzzzzXzzzzzzzzzzzzzzzz",
+		"zzzzzXzzzzzzzzzzzzzzzzz",
+		"zzzzXzzzzzzzzzzzzzzzzzz",
+		"zzzXzzzzzzzzzzzzzzzzzzz",
+		"zzXzzzzzzzzzzzzzzzzzzzz",
+		"zXzzzzzzzzzzzzzzzzzzzzz",
+		"Xzzzzzzzzzzzzzzzzzzzzzz"};
 
-	return (0);
+	/*Get the length of the array*/
+	numStrings = sizeof(strings) / sizeof(strings[0]);
+
+	/*Shuffle the array*/
+	shuffleArray((char **)strings, numStrings);
+
+	/*Print the randomly selected string*/
+	printf("%s", strings[0]);
+
+	return 0;
 }
