9ce9b3dbfa9745e3bbec7c863efff48fa80f2ec6
Emad Anwer <emadanwer.official@gmail.com>
2023-07-07 17:38:30 +0200

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,8 +11,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table;
 	unsigned int i;
 
-	if (size == 0)
-		return NULL;
 	hash_table = malloc(sizeof(hash_table_t *));
 	if (!hash_table)
 		return (NULL);
