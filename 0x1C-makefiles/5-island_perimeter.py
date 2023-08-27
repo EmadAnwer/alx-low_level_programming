1d82fbad4573a44718dde6d6fb30cb9e0c1ac67b
Emad Anwer <emadanwer.official@gmail.com>
2023-08-27 21:53:46 +0300

    t5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,19 +5,18 @@
 def island_perimeter(grid):
     """ Island Perimeter"""
     count = 0
-    rows = len(grid)
-    cols = len(grid[0])
-    perimeter = 0
-
-    for i in range(rows):
-        for j in range(cols):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                perimeter += 4  # Start with 4 sides
-
-                # Check neighbors to subtract shared sides
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2  # Subtract top side
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2  # Subtract left side
-
-    return perimeter
+                try:
+                    if grid[i + 1][j] == 0:
+                        count += 1
+                    if grid[i - 1][j] == 0:
+                        count += 1
+                    if grid[i][j - 1] == 0:
+                        count += 1
+                    if grid[i][j + 1] == 0:
+                        count += 1
+                except Exception as e:
+                    count += 1
+    return count
