e81458c724353881fdea3f4869b946cf92f42c0f
Emad Anwer <emadanwer.official@gmail.com>
2023-08-27 21:30:16 +0300

    task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Island Perimeter"""
+
+
+def island_perimeter(grid):
+    """ Island Perimeter"""
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                try:
+                    if not grid[i + 1][j]:
+                        count += 1
+                    if not grid[i - 1][j]:
+                        count += 1
+                    if not grid[i][j - 1]:
+                        count += 1
+                    if not grid[i][j + 1]:
+                        count += 1
+                except Exception as e:
+                    count += 1
+    return count
