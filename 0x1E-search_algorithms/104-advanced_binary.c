cda5c9f298a14a0c6400b12b755c54047ce61925
Emad Anwer <emadanwer.official@gmail.com>
2023-10-10 19:46:44 +0300

    adv 4

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,42 +1,43 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary_helper - searches for a value in a sorted array of integers
+ * binary_search_recursive - searches for a value in a sorted array of integers
  * @array: array of ints
- * @l: left index
- * @r: right index
+ * @left: left index
+ * @right: right index
  * @value: int value we are looking for
  * Return: Always EXIT_SUCCESS
  */
-int advanced_binary_helper(int *array, int l, int r, int value)
+int binary_search_recursive(int *array, int left, int right, int value)
 {
-	int i, m;
+	int i;
 
-	if (l > r || l < 0)
-		return (-1);
-
-	printf("Searching in array:");
-	for (i = l; i <= r; i++)
+	if (left <= right)
 	{
-		if (i == l)
-			printf(" %d", array[i]);
-		else
-			printf(", %d", array[i]);
-	}
-	printf("\n");
+		int mid = left + (right - left) / 2;
 
-	m = (l + r) / 2;
-	if (array[m] == value)
-	{
-		if (m == l)
-			return (m);
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			if (i == left)
+				printf("%d", array[i]);
+			else
+				printf(", %d", array[i]);
+		}
+		printf("\n");
+
+		if (array[mid] == value)
+		{
+			if (mid == left || array[mid - 1] != value)
+				return (mid);
+			return (binary_search_recursive(array, left, mid - 1, value));
+		}
+		else if (array[mid] < value)
+			return (binary_search_recursive(array, mid + 1, right, value));
 		else
-			return (advanced_binary_helper(array, l, m, value));
+			return (binary_search_recursive(array, left, mid - 1, value));
 	}
-	else if (array[m] < value)
-		return (advanced_binary_helper(array, m + 1, r, value));
-	else
-		return (advanced_binary_helper(array, l, m - 1, value));
+	return (-1);
 }
 
 /**
@@ -48,8 +49,8 @@ int advanced_binary_helper(int *array, int l, int r, int value)
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
-	return (advanced_binary_helper(array, 0, size - 1, value));
+	return (binary_search_recursive(array, 0, size - 1, value));
 }
